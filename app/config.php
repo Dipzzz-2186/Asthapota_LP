96798a074b3f173c62139946fb18a53ebc2c788a
Kiwathh <adnanfadhillah.id@gmail.com>
2026-02-09 11:39:15 +0700

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 $CONFIG = [
     'db_host' => '127.0.0.1',
-    'db_name' => 'fcom_Asthapora_LP',
+    'db_name' => 'Asthapora_LP',
     'db_user' => 'root',
-    'db_pass' => 'adnan2705',
+    'db_pass' => 'Nadif-24',
     'db_charset' => 'utf8mb4',
     'admin_email' => 'admin@temupadel.local',
     // Default admin password: admin123 (change after first login)
