65cf8ccc4db037fc04c9630c00a6c40448c78ed8
Dipzzz-2186 <silviahneneng2007@gmail.com>
2026-02-10 09:11:27 +0700

    Update home.php

--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@ $packages = $db->query('SELECT * FROM packages ORDER BY id')->fetchAll(PDO::FETC
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Temu Padel</title>
+  <title>Asthapora</title>
   <link rel="stylesheet" href="/assets/css/style.css">
   <style>
     /* Marquee Slider Container */
