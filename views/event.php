2d8bdb07e84aca5db3d85a056681c970d11e65c6
Dipzzz-2186 <silviahneneng2007@gmail.com>
2026-02-15 21:21:53 +0700

    update logo

--- views/event.php
+++ views/event.php
@@ -51,7 +51,7 @@ if (!$sponsorItems) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Home - Temu Padel 2026</title>
-  <link rel="icon" type="image/png" href="/assets/img/AsthaporaLogo.png">
+  <link rel="icon" type="image/png" href="/assets/img/LogoTitleAsthapora.png">
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Anton&family=Manrope:wght@400;500;600;700;800;900&family=Playfair+Display:ital,wght@0,600;0,700;1,500&display=swap');
     @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
